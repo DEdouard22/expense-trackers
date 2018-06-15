c3bbe69f2f7fc69c6bc74740a2226cf13e4ef694
carly harper <carly.harper@thingtech.com>
2018-06-15 15:28:00 -0400

    updated color conditioning

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const models = require('../server/models');
 
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     models.Transaction.findAll({
@@ -17,21 +16,38 @@ router.get('/', function(req, res, next) {
     .then(transactions => {
         res.render('transactions', {
             title: 'Transactions',
-            transactions: transactions
+            transactions: transactions,
         });
     })
 });
 
-router.delete('/delete', function(req, res, next) {
+router.get('/delete/:id', function(req, res, next) {
     const id = req.params.id;
     models.Transaction.destroy({
-        where: {id: 26}
+        where: {id: id}
     })
     .then(transactions => {
         res.redirect('/transactions');
-        // console.log('console logging');
     });
-    res.send('success!');
 });
+
+// router.get('/edit/:id', function(req, res, next) {
+//     const id = req.params.id;
+//     models.Transaction.findById({
+//         where: {id: id}
+//     })
+//     .then(transactions => {
+//         res.render('edittransaction', {
+//             title: 'Edit Transaction'
+//     });
+// });
+
+//add a button to go back to transactions
+
+
+//edit needs to be a get, create a new view with the edit details
+//when click save, use findbyID to find the one you need to update,
+//in the then function, send the transaction and do transaction.update.
+//that takes an object of the things you wnat to update
 
 module.exports = router;
