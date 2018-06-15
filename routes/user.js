c4d94a7e5023f8224b9d52d414da5ad3f51b42b9
DEdouard22 <dedouard@live.com>
2018-06-15 16:39:51 -0400

    added a user route to handle posts that are executed from the Auth.js, changed the action path on the login.hbs page and edited the user.hbs page to display email, github id and facebook id

--- routes/user.js
+++ routes/user.js
@@ -19,4 +19,69 @@ router.get('/:id', function(req, res, next) {
     });
 });
 
+// Testing redirect from succesfull login
+router.post('/', function (req, res) {
+  //Checks for Login Method
+  console.log(req.body);
+  if (req.body.email !== null) {
+    console.log('Some one logged in with an email');
+    models.User.findOne({
+      where: {
+        email: req.body.email,
+        password: req.body.password
+      },
+      include: [
+        models.Paymentmethod
+      ]
+    })
+    .then(user => {
+      res.render('user', {
+        title: "User's Page",
+        user: user,
+        methods: user.Paymentmethods
+      });
+    })
+  }
+  else if (req.body.github_id !== null) {
+    console.log('Someone logged in with GitHub');
+    models.User.findOne({
+      where: {
+        github_id: req.body.github_id
+      },
+      include: [
+        models.Paymentmethod
+      ]
+    })
+    .then(user => {
+      res.render('user', {
+        title: "User's Page",
+        user: user,
+        methods: user.Paymentmethods
+      });
+    })
+  }
+  else if (req.body.facebook_id !== null) {
+    console.log('Someone logged in with Facebook');
+    models.User.findOne({
+      where: {
+        facebook_id: req.body.facebook_id
+      },
+      include: [
+        models.Paymentmethod
+      ]
+    })
+    .then(user => {
+      res.render('user', {
+        title: "User's Page",
+        user: user,
+        methods: user.Paymentmethods
+      });
+    })
+  }
+  else{
+    res.redirect('/login');
+  }
+
+});
+
 module.exports = router;
